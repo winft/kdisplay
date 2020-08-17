05cc25d2206653176338536545c44e47def3bc60
Roman Gilg <subdiff@gmail.com>
2020-08-17 15:25:53 +0200

    feat(kcm): provide a standalone app
    In order for KDisplay to be a viable solution in other desktop environments
    than KDE provide the KCM as a standalone app.
    
    The app can be opened by the provided .desktop-file or form terminal by the
    command `kdisplay`.
    
    This is based on the solution created for KDE Connect [1].
    
    [1] https://invent.kde.org/network/kdeconnect-kde/-/tree/master/settings

---
+++ kcm/app/main.cpp
@@ -0,0 +1,52 @@
+/*
+ * SPDX-FileCopyrightText: 2018 Nicolas Fella <nicolas.fella@gmx.de>
+ * SPDX-FileCopyrightText: 2020 Roman Gilg <subdiff@gmail.com>
+ *
+ * SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+ */
+
+#include <QApplication>
+#include <QCommandLineParser>
+#include <QStyle>
+
+#include <KAboutData>
+#include <KCMultiDialog>
+#include <KLocalizedString>
+
+int main(int argc, char** argv)
+{
+    QApplication app(argc, argv);
+    KAboutData about(QStringLiteral("kdisplay"),
+                     i18n("KDisplay"),
+                     QStringLiteral(KDISPLAY_VERSION),
+                     i18n("KDisplay"),
+                     KAboutLicense::GPL,
+                     i18n("Copyright (C) 2020 Roman Gilg"));
+    about.addAuthor(QStringLiteral("Roman Gilg"));
+    about.setBugAddress("https://gitlab.com/kwinft/kdisplay/-/issues");
+    KAboutData::setApplicationData(about);
+
+    QCommandLineParser parser;
+    parser.addOption(QCommandLineOption(
+        QStringLiteral("args"), i18n("Arguments for the config module"), QStringLiteral("args")));
+
+    about.setupCommandLine(&parser);
+    parser.process(app);
+    about.processCommandLine(&parser);
+
+    KCMultiDialog* dialog = new KCMultiDialog;
+    dialog->addModule(QStringLiteral("kcm_kdisplay"), {parser.value(QStringLiteral("args"))});
+
+    auto style = dialog->style();
+    dialog->setContentsMargins(style->pixelMetric(QStyle::PM_LayoutLeftMargin),
+                               style->pixelMetric(QStyle::PM_LayoutTopMargin),
+                               style->pixelMetric(QStyle::PM_LayoutRightMargin),
+                               style->pixelMetric(QStyle::PM_LayoutBottomMargin));
+
+    dialog->setAttribute(Qt::WA_DeleteOnClose);
+    dialog->show();
+
+    app.setQuitOnLastWindowClosed(true);
+
+    return app.exec();
+}
