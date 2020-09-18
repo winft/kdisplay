920c8c78a9a5f29ca1eb13d95e1b82818e84d4ce
Roman Gilg <subdiff@gmail.com>
2020-09-18 23:12:02 +0200

    refactor(kded): remove output file handling
    As with configs output data is written to files in Disman. Remove the unused
    functionality and merge what is still needed.

--- kded/config.cpp
+++ kded/config.cpp
@@ -18,7 +18,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config.h"
 
 #include "kdisplay_daemon_debug.h"
-#include "output.h"
 
 #include <QDir>
 #include <QFile>
@@ -46,6 +45,43 @@ bool Config::autoRotationRequested() const
     return false;
 }
 
+Disman::Output::Rotation orientationToRotation(QOrientationReading::Orientation orientation,
+                                               Disman::Output::Rotation fallback)
+{
+    using Orientation = QOrientationReading::Orientation;
+
+    switch (orientation) {
+    case Orientation::TopUp:
+        return Disman::Output::Rotation::None;
+    case Orientation::TopDown:
+        return Disman::Output::Rotation::Inverted;
+    case Orientation::LeftUp:
+        return Disman::Output::Rotation::Right;
+    case Orientation::RightUp:
+        return Disman::Output::Rotation::Left;
+    case Orientation::Undefined:
+    case Orientation::FaceUp:
+    case Orientation::FaceDown:
+        return fallback;
+    default:
+        Q_UNREACHABLE();
+    }
+}
+
+bool updateOrientation(Disman::OutputPtr& output, QOrientationReading::Orientation orientation)
+{
+    if (output->type() != Disman::Output::Type::Panel) {
+        return false;
+    }
+    const auto currentRotation = output->rotation();
+    const auto rotation = orientationToRotation(orientation, currentRotation);
+    if (rotation == currentRotation) {
+        return true;
+    }
+    output->set_rotation(rotation);
+    return true;
+}
+
 void Config::setDeviceOrientation(QOrientationReading::Orientation orientation)
 {
     for (auto& [key, output] : m_data->outputs()) {
@@ -56,7 +92,7 @@ void Config::setDeviceOrientation(QOrientationReading::Orientation orientation)
         if (output->auto_rotate_only_in_tablet_mode() && !m_data->tablet_mode_engaged()) {
             finalOrientation = QOrientationReading::Orientation::TopUp;
         }
-        if (Output::updateOrientation(output, finalOrientation)) {
+        if (updateOrientation(output, finalOrientation)) {
             // TODO: call Layouter to find fitting positions for other outputs again
             return;
         }
