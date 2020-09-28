d5bcdd4b2026abbe8fa28a68d862e9d5b59a72cf
Carson Black <uhhadd@gmail.com>
2020-09-28 15:04:12 -0400

    build: extract potfile for kcm
    This allows us to set up Weblate for UI translations.

--- kcm/Messages.sh
+++ kcm/Messages.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-$XGETTEXT `find . -name "*.cpp" -o -name "*.qml"` -o $podir/kcm_kdisplay.pot
+mkdir -p po
+xgettext --from-code=UTF-8 -C --kde -ci18n -ki18n:1 -ki18nc:1c,2 -ki18np:1,2 -ki18ncp:1c,2,3 -ktr2i18n:1 -kI18N_NOOP:1 -kI18N_NOOP2:1c,2 -kI18N_NOOP2_NOSTRIP:1c,2 -kaliasLocale -kki18n:1 -kki18nc:1c,2 -kki18np:1,2 -kki18ncp:1c,2,3 `find . -name "*.cpp" -o -name "*.qml"` -o po/kcm_kdisplay.pot
