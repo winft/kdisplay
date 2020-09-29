b25c633c7d7f98675999eb50df13f30e227f07bd
Roman Gilg <subdiff@gmail.com>
2020-09-29 11:44:14 +0200

    build(kcm): make Messages.sh independent of working direcotry
    To run the script in our CI and easily in locale environments change internally
    to the script location before running any commands relative to the path.

--- kcm/Messages.sh
+++ kcm/Messages.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd ${SCRIPT_DIR}
 mkdir -p po
+
 xgettext --from-code=UTF-8 -C --kde -ci18n -ki18n:1 -ki18nc:1c,2 -ki18np:1,2 -ki18ncp:1c,2,3 -ktr2i18n:1 -kI18N_NOOP:1 -kI18N_NOOP2:1c,2 -kI18N_NOOP2_NOSTRIP:1c,2 -kaliasLocale -kki18n:1 -kki18nc:1c,2 -kki18np:1,2 -kki18ncp:1c,2,3 `find . -name "*.cpp" -o -name "*.qml"` -o po/kcm_kdisplay.pot
